3257ac4bf5741c6570bbc910cfe0ee09ab0fe516
Vladimir <vbondarenko89@gmail.com>
2018-10-28 13:11:16 +0300

    first

---
+++ test.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Class BigNumber
+ */
+class BigNumber
+{
+    /**
+     * Fill the string with zeros
+     *
+     * @param $sBigNumber
+     * @param $iMaxLength
+     * @return string
+     */
+    static function completeZero($sBigNumber, $iMaxLength)
+    {
+        if(strlen($sBigNumber) < $iMaxLength)
+        {
+            $diff = $iMaxLength - strlen($sBigNumber);
+            for ($i = 0; $i < $diff; $i++)
+            {
+                $sBigNumber = '0' . $sBigNumber;
+            }
+        }
+
+        return $sBigNumber;
+    }
+
+    /**
+     * Returns the sum of large numbers in a string.
+     *
+     * @param $sNumberA
+     * @param $sNumberB
+     * @return string
+     */
+    static function sum($sNumberA, $sNumberB)
+    {
+        $a = $sNumberA;
+        $b = $sNumberB;
+
+        $maxLength = strlen($a) >= strlen($b) ? strlen($a) : strlen($b);
+
+        $a = self::completeZero($a, $maxLength);
+        $b = self::completeZero($b, $maxLength);
+
+        $result = '';
+        $buf = 0;
+
+        //Culumn additional
+        for ($i = $maxLength - 1; $i >= 0; $i--) {
+            $aPart = $a[$i] ?? 0;
+            $bPart = $b[$i] ?? 0;
+
+            $sum = (string)((int)$aPart + (int)$bPart + $buf);
+            $buf = 0;
+
+            if (strlen($sum) > 1) {
+                $buf = (int)$sum[0];
+                $sum = (int)$sum[1];
+            }
+
+            $result = $sum . $result;
+        }
+
+        return $result;
+    }
+}
+
+echo BigNumber::sum('123456', '123');
